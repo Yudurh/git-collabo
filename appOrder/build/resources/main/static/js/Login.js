3eb673de362840c3f8e6a8b030db976c816ed930
yemmi0305 <tlroal0305@naver.com>
2023-11-09 23:59:15 +0900

    20231109 yemmi

---
+++ appOrder/build/resources/main/static/js/Login.js
@@ -0,0 +1,41 @@
+function loginAction() {
+  let memberId = document.getElementById("inputId").value;
+  let memberPw = document.getElementById("inputPw").value;
+  console.log("아이디 : " + memberId);
+  console.log("비밀번호 : " + memberPw);
+
+  let params = {
+    loginId: memberId,
+    loginPw: memberPw,
+  };
+  console.log(JSON.stringify(params));
+
+  fetch("/loginAction", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(params),
+  })
+    .then((response) => {
+      console.log("response:" + response);
+      console.log("response:" + JSON.stringify(response));
+    }) //HTTP 응답
+    .then((json) => {
+      //{ status: "ok", result: 5 }
+      console.log("json:" + json);
+      console.log("response:" + JSON.stringify(json));
+
+      if (json.result == 1) {
+        window.location.href = "/main";
+      } else {
+        //로그인 실패
+        alert("로그인 실패입니다.");
+      }
+    }) //실제 데이타
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function joinForm() {
+  window.location.href = "/joinForm";
+}
